1af2c3e15d9ddaedcaab2d1abd91fb54fed1caa0
winson <winson1992@gmail.com>
2017-10-03 11:01:17 +0800

    create module library

---
+++ library/src/main/java/io/github/xiewinson/smoothrefreshlayout/library/SmoothRefreshLayout.java
@@ -0,0 +1,24 @@
+package io.github.xiewinson.smoothrefreshlayout.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by winson on 2017/10/3.
+ */
+
+public class SmoothRefreshLayout extends FrameLayout {
+
+    public SmoothRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public SmoothRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SmoothRefreshLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
